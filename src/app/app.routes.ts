a70a919a05a62e9416cf70d22da37c804e1c82c7
David perez <davidmfc1092@hotmail.com>
2026-02-05 11:01:46 -0500

    progect development

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'todo',
+    pathMatch: 'full',
+  },
+  {
+    path: 'todo',
+    loadComponent: () => import('./pages/todo/todo.page').then( m => m.TodoPage)
+  },
+  {
+    path: 'categories',
+    loadComponent: () => import('./pages/categories/categories.page').then( m => m.CategoriesPage)
+  },
+];
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+
+  firebaseConfig : {
+  apiKey: "AIzaSyAUp5cH--hdlV16AaCyLv6To2AxSEAsD54",
+  authDomain: "ionic-task-9bfb3.firebaseapp.com",
+  projectId: "ionic-task-9bfb3",
+  storageBucket: "ionic-task-9bfb3.firebasestorage.app",
+  messagingSenderId: "495599960792",
+  appId: "1:495599960792:web:8af60162d25ad70c7a4dad",
+  measurementId: "G-QECLRDQ4CE"
+}
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
